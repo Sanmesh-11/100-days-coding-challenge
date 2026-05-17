820afbe24cb128541963cd45d94ca898ef28193c
Sanmesh11 <sanmesh.patil@o365.cctech.co.in>
2026-05-17 12:55:38 +0530

    Upper left

--- Day 13/Upper Left.cpp
+++ Day 13/Upper Left.cpp
@@ -1,49 +1,102 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-// Function to find smallest prime factor
-int smallestPrimeFactor(int n) {
-    
-    // Check from 2 onwards
-    for (int i = 2; i * i <= n; i++) {
-        
-        // If completely divisible
-        if (n % i == 0) {
-            return i;
-        }
-    }
+// DFS function
+void dfs(int i, int j,
+         int n, int m,
+         vector<vector<int>>& grid,
+         vector<vector<bool>>& visited,
+         int& minRow, int& minCol,
+         int& maxRow, int& maxCol) {
 
-    // If no factor found, number itself is prime
-    return n;
+    // Check boundaries
+    if (i < 0 || j < 0 || i >= n || j >= m)
+        return;
+
+    // If cell is 0 or already visited
+    if (grid[i][j] == 0 || visited[i][j])
+        return;
+
+    // Mark visited
+    visited[i][j] = true;
+
+    // Update boundaries
+    minRow = min(minRow, i);
+    minCol = min(minCol, j);
+
+    maxRow = max(maxRow, i);
+    maxCol = max(maxCol, j);
+
+    // Visit 4 directions
+
+    // Down
+    dfs(i + 1, j, n, m, grid, visited,
+        minRow, minCol, maxRow, maxCol);
+
+    // Up
+    dfs(i - 1, j, n, m, grid, visited,
+        minRow, minCol, maxRow, maxCol);
+
+    // Right
+    dfs(i, j + 1, n, m, grid, visited,
+        minRow, minCol, maxRow, maxCol);
+
+    // Left
+    dfs(i, j - 1, n, m, grid, visited,
+        minRow, minCol, maxRow, maxCol);
 }
 
-// Function to solve the problem
-int user_logic(int n) {
+void user_logic(int n, int m, vector<vector<int>>& grid) {
 
-    // Find smallest prime factor
-    int spf = smallestPrimeFactor(n);
+    // Visited array
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-    // If number is prime
-    if (spf == n) {
-        return 1;
+    // Traverse grid
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < m; j++) {
+
+            // New water body found
+            if (grid[i][j] == 1 && !visited[i][j]) {
+
+                // Initialize boundaries
+                int minRow = i;
+                int minCol = j;
+                int maxRow = i;
+                int maxCol = j;
+
+                // Start DFS
+                dfs(i, j, n, m, grid, visited,
+                    minRow, minCol, maxRow, maxCol);
+
+                // Print answer
+                cout << minRow << " "
+                     << minCol << " "
+                     << maxRow << " "
+                     << maxCol << endl;
+            }
+        }
     }
-
-    // If number is even
-    if (n % 2 == 0) {
-        return n / 2;
-    }
-
-    // Odd composite number
-    return 1 + (n - spf) / 2;
 }
 
 int main() {
-    int n;
-    cin >> n;
 
-    int result = user_logic(n);
+    int n, m;
+    cin >> n >> m;
 
-    cout << result << endl;
+    vector<vector<int>> grid(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < m; j++) {
+
+            cin >> grid[i][j];
+        }
+    }
+
+    user_logic(n, m, grid);
 
     return 0;
 }
